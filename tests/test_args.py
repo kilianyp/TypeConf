3fc32cdb6f20b74fc3ee1189a033620dcef6c4c4
Kilian Pfeiffer <kilian.pfeiffer@rwth-aachen.de>
2020-11-27 01:04:41 +0100

    WIP add tests

---
+++ tests/test_args.py
@@ -0,0 +1,82 @@
+from typeconf import BaseConfig
+import unittest.mock
+from typing import Optional, List
+
+
+class NestedNestedConfig(BaseConfig):
+    test : int
+
+class NestedConfig(BaseConfig):
+    test : int
+    nested : NestedNestedConfig
+
+
+class TestConfig(BaseConfig):
+    nested : NestedConfig
+
+def test_nested():
+    testargs = ["_", "--nested.test", "123", "--nested.nested.test", "456"]
+    TestConfig.use_cli()
+    with unittest.mock.patch('sys.argv', testargs):
+        cfg = TestConfig()
+        assert cfg.nested.test == 123
+        assert cfg.nested.nested.test == 456
+
+
+
+class ListConfig(BaseConfig):
+    test : List
+
+
+def test_list_1param():
+    testargs = ["_", "--test", "123"]
+    ListConfig.use_cli()
+    with unittest.mock.patch('sys.argv', testargs):
+        cfg = ListConfig()
+        assert cfg.test == ["123"]
+
+
+def test_list_2param():
+    testargs = ["_", "--test", "123", "456"]
+    ListConfig.use_cli()
+    with unittest.mock.patch('sys.argv', testargs):
+        cfg = ListConfig()
+        assert cfg.test == ["123", "456"]
+
+
+class ListIntConfig(BaseConfig):
+    test : List[int]
+
+
+def test_list_int():
+    testargs = ["_", "--test", "123", "456"]
+    ListIntConfig.use_cli()
+    with unittest.mock.patch('sys.argv', testargs):
+        cfg = ListIntConfig()
+        assert cfg.test == [123, 456]
+
+class OptionalConfig(BaseConfig):
+    test : Optional[str]
+
+def test_optional():
+    testargs = ["_"]
+    OptionalConfig.use_cli()
+    with unittest.mock.patch('sys.argv', testargs):
+        cfg = OptionalConfig()
+        assert cfg.test is None
+
+
+class ListOptionalConfig(BaseConfig):
+    test : Optional[List[int]]
+
+def test_optional_list_int():
+    ListOptionalConfig.use_cli()
+    with unittest.mock.patch('sys.argv', ["_"]):
+        cfg = ListOptionalConfig()
+        cfg.test == []
+
+    testargs = ["_", "--test", "123", "456"]
+    with unittest.mock.patch('sys.argv', testargs):
+        cfg = ListOptionalConfig()
+        assert cfg.test == [123, 456]
+
--- typeconf/__init__.py
+++ typeconf/__init__.py
@@ -1,10 +1,42 @@
 from collections import defaultdict
+import argparse
 from pydantic import BaseModel
 from typing import Dict
 import logging
 from abc import abstractmethod
+import inspect
+from typing import List
 
 logger = logging.getLogger(__name__)
+
+
+def fields2args(parser, fields, prefix=''):
+    """
+    Avoids any parsing functionality --> all optional
+
+    args:
+        fields: Pydantic fields
+    """
+    for key, f in fields.items():
+        # TODO check for annotation
+        # Work around
+        if not inspect.isclass(f.outer_type_) and f.outer_type_.__origin__ == list:
+            # if its not a class, it's probably an annotation instance
+            nargs = '+'
+        else:
+            nargs = None
+
+        if inspect.isclass(f.outer_type_) and issubclass(f.outer_type_, BaseConfig):
+            # TODO add groups
+            fields2args(parser, f.outer_type_.__fields__, prefix + f.name + '.')
+            # TODO this is necessary because otherwise
+            # when instantiating the sub config it will complain
+            # AttributeError: _parser
+            # ALternative would be that each class parses it's own arguments
+            f.outer_type_._parser = None
+        else:
+            parser.add_argument(f'--{prefix}{f.name}', default=f.default, nargs=nargs)
+    return parser
 
 
 class BaseConfig(BaseModel):
@@ -12,9 +44,41 @@ class BaseConfig(BaseModel):
     https://github.com/samuelcolvin/pydantic/issues/2130
     """
     _field_access = defaultdict(int)
+    _parser = None
 
     class Config:
         underscore_attrs_are_private = True
+
+    def __init__(self, **kwargs):
+        if self._parser is not None:
+            cli_args = self._parser.parse_args()
+            # file_cfg = read_cfg(cli_args.config_path)
+            file_cfg = {}
+            # Here needs to be the priority
+            # args over cfg
+            # what about runtime arguments
+
+            def args2dict(dic):
+                r = {}
+                for key, value in dic.items():
+                    depth = key.split('.')
+                    cur = r
+                    for idx, d in enumerate(depth):
+                        if idx == len(depth) - 1:
+                            cur[d] = value
+                        else:
+                            if d not in cur:
+                                cur[d] = {}
+                            cur = cur[d]
+                return r
+
+            r = args2dict(cli_args.__dict__)
+            file_cfg.update(r)
+            # TODO update fields individually
+            file_cfg.update(kwargs)
+            kwargs = file_cfg
+        super().__init__(**kwargs)
+
 
     def __getattribute__(self, item):
         if not item.startswith('_') and item in self.__fields__:
@@ -35,6 +99,11 @@ class BaseConfig(BaseModel):
     def parse(cls, cfg, *args, **kwargs):
         cls = cls.build_config(cfg, *args, **kwargs)
         return cls(**cfg)
+    @classmethod
+    def use_cli(cls):
+        parser = argparse.ArgumentParser(cls.__name__)
+        parser.add_argument('--config_path')
+        cls._parser = fields2args(parser, cls.__fields__)
 
 
 class SelectConfig(BaseConfig):
