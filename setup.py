59717aa04dae25576b554055761b078085e1d78d
Kilian Pfeiffer <kilian.pfeiffer@rwth-aachen.de>
2020-12-28 00:20:25 +0100

    Requirements in separate file

--- setup.py
+++ setup.py
@@ -1,8 +1,18 @@
 import setuptools
 import versioneer
+import os
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
+
+here = os.path.abspath(os.path.dirname(__file__))
+requirements_path = os.path.join(here, 'requirements.txt')
+
+if os.path.isfile(requirements_path):
+    with open(requirements_path, encoding='utf-8') as f:
+        REQUIRED = f.read().split('\n')
+else:
+    REQUIRED = []
 
 setuptools.setup(
     name="typeconf",
@@ -15,10 +25,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/kilsenp/TypeConf",
     packages=setuptools.find_packages(where="src"),
-    install_requires=[
-        'pydantic',
-        'omegaconf'
-    ],
+    install_requires=REQUIRED,
     package_dir={'': 'src'},
     classifiers=[
         "Programming Language :: Python :: 3",
